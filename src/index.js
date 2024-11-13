9ae9115395d53cf6adcb9b5a903428676bf7ec3a
ranyoung <fks1311@gmail.com>
2024-11-13 23:33:56 +0900

    기본 utils 파일 생성

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./utils/Router";
+import { theme } from "./utils/style";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
