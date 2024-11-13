93065a5b69f20bc46f08b52555f719029879f876
ranyoung <fks1311@gmail.com>
2024-11-14 01:22:34 +0900

    인덱스 설정

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./utils/Router";
+import { router } from "./utils/router";
 import { theme } from "./utils/style";
+import reset from "styled-reset";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body {
+    font-family: Roboto_Regular;
+    font-size: 16px;
+    font-weight: 400;
+  }
+`;
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
