c366f93ed458cd0c86f023e976ec7883519f8716
ranyoung <fks1311@gmail.com>
2024-11-14 05:28:14 +0900

    Success 라우터 생성

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,4 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import { Success } from "components/Success";
 
-export const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+export const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/success", element: <Success /> },
+]);
