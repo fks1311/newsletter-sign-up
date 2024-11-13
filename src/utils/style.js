ef9ad491bdb6a0d64d32050c27ae780bc0e048fe
ranyoung <fks1311@gmail.com>
2024-11-13 23:42:58 +0900

    style 속성 지정

--- src/utils/style.js
+++ src/utils/style.js
@@ -1,1 +1,21 @@
-export const theme = {};
+const deviceSizes = {
+  mobile: `375px`,
+  desktop: `1440px`,
+};
+
+const device = {
+  mobile: `screen and (max-width: ${deviceSizes.mobile})`,
+  desktop: `screen and (max-width: ${deviceSizes.desktop})`,
+};
+
+const colors = {
+  tomato: `hsl(4, 100%, 67%)`,
+  charcoal_grey: `hsl(234, 29%, 20%)`,
+  grey: `hsl(231, 7%, 60%)`,
+  white: `hsl(0, 0%, 100%)`,
+};
+
+export const theme = {
+  device,
+  colors,
+};
