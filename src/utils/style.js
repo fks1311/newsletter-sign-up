1ae08fb26bc31b9925318a8268cca282e9d5b102
ranyoung <fks1311@gmail.com>
2024-11-14 05:22:38 +0900

    grey 컬러 추가

--- src/utils/style.js
+++ src/utils/style.js
@@ -10,6 +10,7 @@ const device = {
 
 const colors = {
   tomato: `hsl(4, 100%, 67%)`,
+  dark_slate_grey: `hsl(234, 29%, 20%)`,
   charcoal_grey: `hsl(234, 29%, 20%)`,
   grey: `hsl(231, 7%, 60%)`,
   white: `hsl(0, 0%, 100%)`,
