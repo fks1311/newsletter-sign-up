c05a8d716f0610797800ffbf7386714e647a6d8b
ranyoung <fks1311@gmail.com>
2024-11-17 21:19:14 +0900

    v7 재지정

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -9,11 +9,12 @@ export const router = createBrowserRouter(
   ],
   {
     future: {
-      v7_partialHydration: true,
-      v7_relativeSplatPath: true,
       v7_fetcherPersist: true,
       v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_relativeSplatPath: true,
       v7_skipActionErrorRevalidation: true,
+      v7_startTransition: true,
     },
   }
 );
