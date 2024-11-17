75ca965e6eac52347f7043cd672f549c2dd934d4
ranyoung <fks1311@gmail.com>
2024-11-17 21:14:09 +0900

    v7 적용

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -2,7 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { Success } from "components/Success";
 
-export const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/success", element: <Success /> },
-]);
+export const router = createBrowserRouter(
+  [
+    { path: "/", element: <App /> },
+    { path: "/success", element: <Success /> },
+  ],
+  {
+    future: {
+      v7_partialHydration: true,
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  }
+);
