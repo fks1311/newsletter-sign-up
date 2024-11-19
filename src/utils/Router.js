51845f8dd25c636c01535d5214a49b9cfc01f2d1
ranyoung <fks1311@gmail.com>
2024-11-19 23:16:40 +0900

    V7 삭제

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -2,19 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { Success } from "components/Success";
 
-export const router = createBrowserRouter(
-  [
-    { path: "/", element: <App /> },
-    { path: "/success", element: <Success /> },
-  ],
-  {
-    future: {
-      v7_fetcherPersist: true,
-      v7_normalizeFormMethod: true,
-      v7_partialHydration: true,
-      v7_relativeSplatPath: true,
-      v7_skipActionErrorRevalidation: true,
-      v7_startTransition: true,
-    },
-  }
-);
+export const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/success", element: <Success /> },
+]);
